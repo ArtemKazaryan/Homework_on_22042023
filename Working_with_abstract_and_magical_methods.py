4dba58a0b863e92b659891c5211ee8126b4b0abd
Artem Kazaryan <mirony4@yandex.ru>
2023-04-16 01:41:49 +0300

    Выполнил первое задание (1/4)

--- Working_with_abstract_and_magical_methods.py
+++ Working_with_abstract_and_magical_methods.py
@@ -22,9 +22,71 @@
 # путем изменения ее радиуса (операции + - += -=).
 #
 # Решение:
+from math import pi
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+        self.circumference = round(2 * pi * self.radius, 3)
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __gt__(self, other):
+        return self.circumference > other.circumference
+    def __lt__(self, other):
+        return self.circumference < other.circumference
+    def __ge__(self, other):
+        return self.circumference >= other.circumference
+    def __le__(self, other):
+        return self.circumference <= other.circumference
+
+    def __add__(self, other):
+        return round(2 * pi *(self.radius + other), 3)
+    def __radd__(self, other):
+        return self.__add__(other)
+    def __iadd__(self, other):
+        self.radius += other
+        return round(2 * pi * self.radius, 3)
+
+    def __sub__(self, other):
+        return round(2 * pi * (self.radius - other), 3)
+    def __rsub__(self, other):
+        return self.__sub__(other)
+    def __isub__(self, other):
+        self.radius -= other
+        return round(2 * pi * self.radius, 3)
+
+circle1 = Circle(1)
+circle2 = Circle(2)
+circle3 = Circle(1)
+
+print()
+print('Проверка на равенство радиусов двух окружностей:')
+print(circle1 == circle2)
+print(circle1 == circle3)
+
+print()
+print('Сравнения длин двух окружностей:')
+print(circle1 > circle2)
+print(circle2 > circle1)
+print(circle1 >= circle2)
+print(circle2 >= circle1)
+
+print()
+print('Пропорциональное изменение размеров окружности, путём изменения ее радиуса:')
+summ = circle1 + 1
+print(summ)
+subs = circle2 - 1
+print(subs)
+circle1 += 1
+print(circle1)
+circle2 -= 1
+print(circle2)
 
 
 
+# print(circle1.circumference)
+# print(circle2.circumference)
 
 # Задание 2
 # Создайте класс Complex (комплексное число). Более
